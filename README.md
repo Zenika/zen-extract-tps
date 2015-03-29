a381b565107a49d0971c2fe6e80c499ad571cb8b
Matthieu Lux <matthieu.lux@gmail.com>
2015-03-29 18:40:22 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#zen-extract-tps
+
+Script Node pour extraire les TPs d'une formation (ES6 inside)
---
+++ index.js
@@ -0,0 +1,103 @@
+#!/usr/bin/env node-harmony
+
+// jshint esnext: true
+
+const co = require('co');
+const Promise = require('bluebird');
+const thenify = require('thenify');
+const fs = require('mz/fs');
+const exec = require('mz/child_process').exec;
+const path = require('path');
+const meow = require('meow');
+const os = require('os');
+const mkdirp = thenify(require('mkdirp'));
+const rimraf = thenify(require('rimraf'));
+const cpr = thenify(require('cpr'));
+const archiver = require('archiver');
+
+const cli = meow();
+const trainingName = cli.input[0];
+
+var destinationFile = 'tps.zip';
+if(cli.input.length > 1) {
+  destinationFile = path.resolve(cli.input[1]);
+}
+
+const tmpDirectory = path.resolve(os.tmpdir(), cli.pkg.name);
+const workDirectory = path.resolve(tmpDirectory, 'formation-' + trainingName);
+const resultDirectory = path.resolve(tmpDirectory, 'result');
+const gitUrl = 'git@github.com:Zenika/formation-' + trainingName + '.git';
+
+co(function *() {
+
+  try {
+
+    yield rimraf(tmpDirectory);
+    yield mkdirp(workDirectory);
+
+    console.log('Clonning', 'formation-' + trainingName, '...');
+
+    yield exec('git clone ' + gitUrl, { cwd: tmpDirectory });
+
+    console.log('Clonning done!');
+
+    var branchesOut = yield exec('git branch -r', { cwd: workDirectory });
+    var branches = branchesOut[0].split('\n')
+      .filter(function(branchName) {
+        return /^\s*origin\/tp\d+$/.test(branchName);
+      })
+      .map(function(branchName) {
+        return /^\s*origin\/(tp\d+)$/.exec(branchName)[1];
+      })
+      .sort(function(a, b) {
+        function getNumber(branchName) {
+          return parseInt(/tp(\d+)/.exec(branchName)[1]);
+        };
+        return getNumber(a) - getNumber(b);
+      });
+
+    for (branch of branches) {
+      console.log('Checking out and copy', branch);
+
+      yield exec('git checkout ' + branch, { cwd: workDirectory });
+
+      var tpResultDirectory = path.resolve(resultDirectory, branch);
+      yield mkdirp(tpResultDirectory);
+      yield cpr(workDirectory, tpResultDirectory, {
+        filter: /\.git/
+      });
+    }
+
+    console.log('Zipping...');
+
+    yield zip(resultDirectory, destinationFile);
+
+    console.log('Finished!');
+
+  } catch(error) {
+    console.log('Error', error);
+  }
+
+});
+
+
+const zip = thenify(function zip(sourceFolder, destinationFile, callback) {
+  var file_system = require('fs');
+
+  var output = file_system.createWriteStream(destinationFile);
+  var archive = archiver('zip');
+
+  output.on('close', function () {
+    callback(null);
+  });
+
+  archive.on('error', function(err){
+    callback(err);
+  });
+
+  archive.pipe(output);
+  archive.bulk([
+    { expand: true, cwd: sourceFolder, src: ['**'], dest: '.'}
+  ]);
+  archive.finalize();
+});
